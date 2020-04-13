508a3a3a17f0af3e16565ecd2daa41ca6fa57716
wesd724 <wesd723@naver.com>
2020-04-14 02:32:07 +0900

    init

---
+++ App.js
@@ -0,0 +1,40 @@
+import React, { Component, useState, useEffect } from 'react';
+import { StyleSheet, Text, View, StatusBar, Platform } from 'react-native';
+import { WebView } from 'react-native-webview';
+import Constants from 'expo-constants';
+import * as Location from 'expo-location';
+
+export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  });
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+//https://docs.expo.io/versions/latest/sdk/location/
+
+  return (
+    <View style={{flex: 1}}>
+        <StatusBar hidden={true}/>
+        <WebView
+          source={{ uri: 'https://wesd724.github.io/src/kbMap/map.html' }}
+          style={{ flex: 1 }}
+        />
+      </View>
+  );
+}
